3b4bc013f5bf2f6fec534a10188cbfd398ac5643
arthur <arthurexterkoetter@vonq.com>
2022-04-26 09:37:32 -0300

    add guard for none values

--- igb_client/parse.py
+++ igb_client/parse.py
@@ -22,13 +22,28 @@ def parse_igb_xml_payload(payload: Dict) -> Dict:
         # end condition
         return payload
 
+    print(type(payload))
     for key, value in payload.items():
         if key.lower() in ["facets", "credentials", "options", "params", "rules"]:
             # only a few tags are containers in IGB's XSD
             singular_key = key.rstrip("s")
+            if not payload.get(key):
+                continue
             payload[key] = [
                 parse_igb_xml_payload(item[singular_key]) for item in payload[key]
             ]
         else:
             payload[key] = parse_igb_xml_payload(payload[key])
     return payload
+
+
+p = {
+    "description": "None",
+    "label": "id",
+    "name": "id",
+    "options": None,
+    "sort": 20,
+    "url": "None",
+}
+
+parse_igb_xml_payload(p)
