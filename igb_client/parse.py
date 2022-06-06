7737a61ca20196ce36b5e4b9a4f9450d07d40d4e
Marian Craciun <mariancraciun1983@users.noreply.github.com>
2022-06-06 21:31:43 +0300

    HAPI-935 - fix parsing with missing keys remapping (#6)

--- igb_client/parse.py
+++ igb_client/parse.py
@@ -28,12 +28,11 @@ def parse_igb_xml_payload(payload: Dict) -> Dict:
             singular_key = key.rstrip("s")
             if not payload.get(key):
                 continue
-            payload[key] = []
-            for item in payload[key]:
-                if singular_key in item:
-                    payload[key].append(
-                        parse_igb_xml_payload(item[singular_key])
-                    )
+            payload[key] = [
+                parse_igb_xml_payload(item[singular_key])
+                for item in payload[key]
+                if singular_key in item
+            ]
         else:
             payload[key] = parse_igb_xml_payload(payload[key])
     return payload
